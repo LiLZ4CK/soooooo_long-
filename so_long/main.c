35ce1694691e52a0a8750f11e819302c58fc32f0
LiLZ4CK <94322451+LiLZ4CK@users.noreply.github.com>
2022-01-06 03:20:43 +0100

    Add files via upload

--- so_long/main.c
+++ so_long/main.c
@@ -6,7 +6,7 @@
 /*   By: zwalad <zwalad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 17:14:38 by zwalad            #+#    #+#             */
-/*   Updated: 2022/01/06 01:15:18 by zwalad           ###   ########.fr       */
+/*   Updated: 2022/01/06 03:15:59 by zwalad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ int map_check(int num, char **map)
 char	**map(int argc, char **argv, char **map)
 {
 	int		i;
+	int		j;
 	char	*str;
 
 	i = 0;
@@ -63,20 +64,26 @@ char	**map(int argc, char **argv, char **map)
 	{
 		map[i] = malloc(ft_strlen(str) + 1);
 		map[i] = ft_strdup(str);
+		map[i+1] = NULL;
 		printf("%s", map[i]);
 		str = get_next_line(fd, str);
-		if (str == NULL)
-		{
-			free(str);
-			str = NULL;
-			return (NULL);
-		}
-		
 		if (!map)
 		{
 			free(str);
 			str = NULL;
 			return (NULL);				
+		}
+		i++;
+	}
+	i = 0;
+	int l = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			printf("%c", map[i][j]);
+			j++;
 		}
 		i++;
 	}
@@ -152,6 +159,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 	int j = 0;
 	char	**maap;
+	
 	if (!map_check(argc, argv))
 		{
 			exit (1);
@@ -159,18 +167,32 @@ int main(int argc, char *argv[])
 		}
 	printf("%s\n", "test5");
 	maap = map(argc, argv, maap);
-	/* printf("/.............../\n");
 	while (maap[i])
 	{
-		printf("%s", maap[i]);
+		j = 0;
+		while (maap[i][j])
+		{
+			printf("%c", maap[i][j]);
+			j++;
+		}
 		i++;
-	} */
+	}
+	printf("%s\n", "check0");
+	j = ft_strlen(maap[0]);
+	printf("%s\n", "check1");
 	p.mlx_ptr = mlx_init();
-	p.win_ptr = mlx_new_window(p.mlx_ptr, 1440, 1020, "test");
+	printf("%s\n", "check2");
+	p.win_ptr = mlx_new_window(p.mlx_ptr, j * 20, i * 20, "test");
+	printf("%s\n", "check3");
 	p.player_path = "./img/nft80x80.xpm";
+	printf("%s\n", "check4");
 	p.player = mlx_xpm_file_to_image(p.mlx_ptr, p.player_path, &p.width, &p.height);
+	printf("%s\n", "check5");
 	mlx_key_hook(p.win_ptr, ft_mouve, &p);
+	printf("%s\n", "check6");
 	mlx_hook(p.win_ptr, 17, (1L<<17), mouse, &p);
+	printf("%s\n", "check7");
 	mlx_loop(p.mlx_ptr);
+	printf("%s\n", "check8");
 	return (0);
 }
